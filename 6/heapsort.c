3df2b0558379c9cafe4c289aa55848077304a8f1
Dongkyoo <wind.dong.dream@gmail.com>
2020-06-19 20:34:34 +0900

    6 완성

--- 6/heapsort.c
+++ 6/heapsort.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "person.h"
 //필요한 경우 헤더 파일과 함수를 추가할 수 있음
+
+int page_num;
+int record_num;
+int heap_size;
+
+typedef struct Header {
+     int pageNum;
+     int recordNum;
+     int deletedPage;
+     int deletedRecord;
+} Header;
 
 // 과제 설명서대로 구현하는 방식은 각자 다를 수 있지만 약간의 제약을 둡니다.
 // 레코드 파일이 페이지 단위로 저장 관리되기 때문에 사용자 프로그램에서 레코드 파일로부터 데이터를 읽고 쓸 때도
@@ -15,7 +28,8 @@
 //
 void readPage(FILE *fp, char *pagebuf, int pagenum)
 {
-
+	fseek(fp, SEEK_SET, pagenum * PAGE_SIZE);
+	fread(pagebuf, PAGE_SIZE, 1, fp);
 }
 
 //
@@ -23,7 +37,34 @@ void readPage(FILE *fp, char *pagebuf, int pagenum)
 //
 void writePage(FILE *fp, const char *pagebuf, int pagenum)
 {
+	fseek(fp, SEEK_SET, pagenum * PAGE_SIZE);
+	fwrite(pagebuf, PAGE_SIZE, 1, fp);
+}
 
+void unpack(Person *p, char *record) {
+	char buf[RECORD_SIZE];
+	char *cp;
+
+	memcpy(buf, record, RECORD_SIZE);
+	memset(p, 0, sizeof(Person));
+	
+	cp = strtok(buf, "#");
+	if (cp == NULL) {
+		p->sn[0] = '*';
+		return;
+	}
+
+	strcpy(p->sn, cp);
+	strcpy(p->name, strtok(NULL, "#"));
+	strcpy(p->age, strtok(NULL, "#"));
+	strcpy(p->addr, strtok(NULL, "#"));
+	strcpy(p->phone, strtok(NULL, "#"));
+	strcpy(p->email, strtok(NULL, "#"));
+}
+
+void pack(Person *p, char *record) {
+	sprintf(record, "%s#%s#%s#%s#%s#%s", p->sn, p->name, p->age, p->addr, p->phone, p->email);
+	record[strlen(record)] = '#';
 }
 
 //
@@ -32,7 +73,93 @@ void writePage(FILE *fp, const char *pagebuf, int pagenum)
 //
 void buildHeap(FILE *inputfp, char **heaparray)
 {
+	char buf[PAGE_SIZE];
+	char record_buf[RECORD_SIZE];
+	int record_per_page = PAGE_SIZE / RECORD_SIZE;
+	Person p, p2;
+	int curSize = 1;
+	int parent, current;
+	int count = 0;
+	heap_size = 0;
 
+	for (int index = 1; index < page_num; index++) {
+		readPage(inputfp, buf, index);
+
+		for (int i = 0; i < record_per_page; i++) {
+			unpack(&p, buf + i * RECORD_SIZE);
+			count++;
+			if (p.sn[0] == '*')
+				continue;
+
+			heap_size++;
+			if (count > record_num)
+				break;
+
+			heaparray[curSize] = malloc(RECORD_SIZE);
+			pack(&p, heaparray[curSize]);
+
+			current = curSize;
+			parent = current / 2;
+			while (parent > 0) {
+				unpack(&p, heaparray[current]);
+				unpack(&p2, heaparray[parent]);
+
+				if (strcmp(p.sn, p2.sn) < 0) {
+					memcpy(record_buf, heaparray[current], RECORD_SIZE);
+					memcpy(heaparray[current], heaparray[parent], RECORD_SIZE);
+					memcpy(heaparray[parent], record_buf, RECORD_SIZE);
+
+					current = parent;
+					parent = current / 2;
+				} else {
+					break;
+				}
+			}
+			curSize++;
+		}
+	}
+}
+
+void pop(char **heaparray) {
+	int current, left_child, right_child, min_child;
+	Person p1, p2;
+	char buf[RECORD_SIZE];
+
+	current = 1;
+	memcpy(heaparray[1], heaparray[heap_size], RECORD_SIZE);
+	heap_size--;
+
+	while (current <= heap_size) {
+		left_child = current * 2;
+		right_child = left_child + 1;
+
+		if (left_child > heap_size)
+			break;
+
+		if (right_child <= heap_size) {
+			unpack(&p1, heaparray[left_child]);
+			unpack(&p2, heaparray[right_child]);
+
+			// left가 더 작음
+			if (strcmp(p1.sn, p2.sn) < 0) {
+				min_child = left_child;
+			} else {
+				min_child = right_child;
+			}
+		} else {
+			min_child = left_child;
+		}
+
+		unpack(&p1, heaparray[current]);
+		unpack(&p2, heaparray[min_child]);
+
+		if (strcmp(p1.sn, p2.sn) < 0)
+			break;
+
+		memcpy(buf, heaparray[current], RECORD_SIZE);
+		memcpy(heaparray[current], heaparray[min_child], RECORD_SIZE);
+		memcpy(heaparray[min_child], buf, RECORD_SIZE);
+	}
 }
 
 //
@@ -42,14 +169,75 @@ void buildHeap(FILE *inputfp, char **heaparray)
 //
 void makeSortedFile(FILE *outputfp, char **heaparray)
 {
+	int record_per_page = PAGE_SIZE / RECORD_SIZE;
+	char buf[PAGE_SIZE];
+	int num = 1;
+	char *cp;
 
+	// TODO: header 
+	Header header;
+	header.pageNum = (heap_size - 1) / record_per_page + 2;
+	header.recordNum = heap_size;
+	header.deletedPage = -1;
+	header.deletedRecord = -1;
+
+	memset(buf, 0xff, sizeof(buf));
+	memcpy(buf, &header, sizeof(Header));
+	writePage(outputfp, buf, 0);
+
+	while (heap_size) {
+		cp = buf;
+
+		for (int i = 0; i < record_per_page && heap_size; i++) {
+			cp = stpcpy(cp, heaparray[1]);
+			pop(heaparray);
+		}
+
+		while (cp < buf + PAGE_SIZE) {
+			*cp = 0xff;
+			cp++;
+		}
+
+		writePage(outputfp, buf, num);
+		num++;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	FILE *inputfp;	// 입력 레코드 파일의 파일 포인터
 	FILE *outputfp;	// 정렬된 레코드 파일의 파일 포인터
+	char buf[PAGE_SIZE];
 
+	if (argc != 4)
+		return -1;
 
-	return 1;
+	if (argv[1][0] != 's')
+		return -1;
+
+	inputfp = fopen(argv[2], "r");
+	outputfp = fopen(argv[3], "w");
+
+	memset(buf, 0xff, sizeof(buf));
+	readPage(inputfp, buf, 0);
+
+	int *ip = (int *) buf;
+	page_num = *ip;
+
+	ip++;
+	record_num = *ip;
+	char **heaparray = calloc(page_num, sizeof(char *));
+
+	buildHeap(inputfp, heaparray);
+	Person *p = malloc(sizeof(Person));
+	for (int i = 1; i <= heap_size; i++) {
+		if (heaparray[i] == NULL)
+			break;
+
+		unpack(p, heaparray[i]); 
+	}
+
+	makeSortedFile(outputfp, heaparray);
+
+	return 0;
 }
